1283b12d397742cc1b3a071faa2c5313f58abd1a
FakiriYassine <yassineyassine0012@gmail.com>
2023-03-08 10:51:54 +0100

    Phase 4 : plusieurs pages.

---
+++ src/Controller/VoyagesController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VoyagesController extends AbstractController {
+    
+    /**
+     * @Route("/voyages", name="voyages")
+     * @return Response
+     */
+    public function index(): Response{
+        return $this->render("pages/voyages.html.twig");
+    }
+}
+  

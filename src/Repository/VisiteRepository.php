8ca4877218556f8323e859799ab15d1ddfe26de7
FakiriYassine <yassineyassine0012@gmail.com>
2023-03-09 13:41:06 +0100

    Phase 9 : ORM (tri des enregistrements

--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -38,6 +38,18 @@ class VisiteRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
+    /**
+     * Retourne toutes les visites triées sur le champ
+     * @param type $champ
+     * @param type $ordre
+     * @return Visite[]
+     */
+    public function findAllOrderBy($champ,$ordre):array{
+        return $this->createQueryBuilder('v')
+                ->orderBy('v.'.$champ, $ordre)
+                ->getQuery()
+                ->getResult();
+    }
 
 //    /**
 //     * @return Visite[] Returns an array of Visite objects
